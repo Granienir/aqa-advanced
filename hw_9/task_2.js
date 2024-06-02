eaf868c474c82d40597ba288715166b1d8d93f70
Illia_Hnitetskyi <ilya.13.654@gmail.com>
2024-06-02 17:11:41 +0300

    hw_9

---
+++ hw_9/task_2.js
@@ -0,0 +1,9 @@
+const book = {
+  title: "Harry Potter and the Philosophers Stone",
+  author: "J. K. Rowling",
+  year: 1995,
+};
+
+const { title, author, year } = book;
+
+console.log(`The "${title}" was written by ${author} in ${year}`);
